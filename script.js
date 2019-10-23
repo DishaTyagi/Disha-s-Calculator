b455e5acd3f3220f665af43d2f85941811adc083
DishaTyagi <omgdisha@gmail.com>
2019-10-23 20:55:34 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,27 @@
+window.onload=function(){
+
+    let textbox= document.getElementById('textbox')
+
+    display=function(val){
+        textbox.value+=val;
+
+    }
+
+    clr=function(){
+        textbox.value=''
+    }
+
+    res=function(){
+        let x=textbox.value;
+        let y=eval(x);
+        textbox.value=y;
+    }
+
+    backspace= function(){
+        // var lastchar= textbox.value[textbox.value.length - 1];
+        // textbox.value-= lastchar;
+        
+        textbox.value= textbox.value.slice(0, textbox.value.length - 1);
+    }
+
+}
